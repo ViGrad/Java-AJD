5665ae12c701d818f01f6d98ce9d25e16f2984d2
Vincent G <vincent.grandiere@gmail.com>
2019-05-17 16:53:41 +0200

    renames + npx

---
+++ services/client-services/src/main/java/com/example/appengine/demos/springboot/Repositories/ClientRepository.java
@@ -0,0 +1,68 @@
+package com.example.appengine.demos.springboot.Repositories;
+
+import com.google.appengine.api.datastore.*;
+
+import java.util.Date;
+import java.util.List;
+
+public class ClientRepository {
+    DatastoreService dataStore;
+
+    public ClientRepository() {
+        dataStore = DatastoreServiceFactory.getDatastoreService();
+    }
+
+    public List<Entity> Get() {
+        Query q = new Query("Client");
+        List<Entity> results = dataStore.prepare(q).asList(FetchOptions.Builder.withDefaults());
+        return results;
+    }
+
+    public Entity GetById(long id) throws Exception {
+        Entity result;
+        Key clientKey = KeyFactory.createKey("Client", id);
+
+        try {
+            result = dataStore.get(clientKey);
+        }
+        catch (Exception ex) {
+            throw new Exception("Unknown ID", ex);
+        }
+
+        return result;
+    }
+
+    public long Create(String firstName, String lastName, String password) {
+        Entity client = new Entity("Client");
+
+        client.setProperty("firstName", firstName);
+        client.setProperty("lastName", lastName);
+        client.setProperty("password", password);
+        client.setProperty("registerDate", new Date());
+        client.setProperty("active", false);
+        dataStore.put(client);
+
+        return client.getKey().getId();
+    }
+
+    public void Modify(long id, String firstName, String lastName) throws Exception {
+        Transaction txn = dataStore.beginTransaction();
+
+        try {
+            Entity client = GetById(id);
+            if (!firstName.isEmpty()) {
+                client.setProperty("firstName", firstName);
+            }
+
+            if (!lastName.isEmpty()) {
+                client.setProperty("lastName", lastName);
+            }
+            dataStore.put(txn, client);
+            txn.commit();
+        }
+        catch (Exception ex) {
+            txn.rollback();
+            throw ex;
+        }
+    }
+}
